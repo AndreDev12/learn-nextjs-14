2dc0dadfcf91f69eb45315c82f1448eb40bdfa73
André Torres <andretorresjava@gmail.com>
2023-11-07 22:35:12 -0500

    Chapter 2: CSS style

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+import Link from 'next/link';
+
 import AcmeLogo from '@/app/ui/acme-logo';
 
-import Link from 'next/link';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
